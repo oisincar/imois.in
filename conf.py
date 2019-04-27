2a6cd299bbaf2f43dfcf5448272db98b7d2460ad
oisincar <oisincar@gmail.com>
2019-04-27 02:05:30 +0100

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Oisin Carroll"  # (translatable)
 BLOG_TITLE = "im_oisin"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "/"
+SITE_URL = "https://imois.in/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://imois.in/"
