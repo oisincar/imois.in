5e40e5f833ec9993321c910f91190f69ef751f54
oisincar <oisincar@gmail.com>
2019-12-01 02:31:45 +0000

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -1212,8 +1212,10 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 EXTRA_HEAD_DATA = """
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
 <link href="https://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" rel="stylesheet" type="text/css">
 """
+
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
