165d71e651e6fc5157c2d536b6ee46d8729d54ac
oisincar <oisincar@gmail.com>
2020-05-28 17:27:26 +0100

    Nikola auto commit.
    Source commit: b3525b3a2bbc2b8b5b31faff91bb5ae589c51757
    Nikola version: 8.0.3

--- assets/js/card_network/fk_network.js
+++ assets/js/card_network/fk_network.js
@@ -1,36 +1,35 @@
-is_anim = false;
-currentPos = 0;
 
-console_output = [""];
-
-function update() {
-    if (!is_anim) return;
-
-    if (currentPos >= console_output.length) {
-        is_anim = false;
-        return;
-    }
-    $("#div1").text("> ./card_network" + console_output[currentPos]);
-    currentPos++;
-}
-
-$.get("../../misc/card_network/out.txt", function(data){
-    d2 = data.split("[9A");
-    console_output = d2;
-    console.log("Loaded!");
-});
-
-
-$("button").click(function(){
-    console.log("Clicked")
-
-    var loc = window.location.pathname;
-    var dir = loc.substring(0, loc.lastIndexOf('/'));
-    console.log(loc);
-    console.log(dir);
-
-    is_anim = true;
+$(document).ready(function() {
+    is_anim = false;
     currentPos = 0;
-});
+    console_output = [""];
 
-setInterval(function() { update(); }, 0.15);
+    // Toggle button on click
+    var btn = $(".button-pp");
+    btn.click(function() {
+        is_anim = !is_anim;
+        btn.toggleClass("paused");
+        return false;
+    });
+
+    function update() {
+        if (!is_anim) return;
+
+        if (currentPos >= console_output.length) {
+            is_anim = false;
+            btn.toggleClass("paused");
+            currentPos = 0;
+            return;
+        }
+        $("#div1").text("> ./card_network" + console_output[currentPos]);
+        currentPos++;
+    }
+
+    $.get("../../misc/card_network/out.txt", function(data){
+        d2 = data.split("[9A");
+        console_output = d2;
+        console.log("Loaded!");
+    });
+
+    setInterval(function() { update(); }, 0.15);
+});
