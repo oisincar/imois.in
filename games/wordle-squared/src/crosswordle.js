2d4eb695d7faa89244866404f4d675f62f4d75c8
oisincar <oisincar@gmail.com>
2023-01-19 17:41:35 +0100

    Nikola auto commit.
    Source commit: d3d0bec364ba705efbcce57ea4dd7c37a6f4c3d9
    Nikola version: 8.1.3

--- games/wordle-squared/src/crosswordle.js
+++ games/wordle-squared/src/crosswordle.js
@@ -255,7 +255,6 @@ let clipboard = new ClipboardJS(document.getElementById('resultsModelCopyButton'
   },
 });
 
-// TODO: Show popover after clicking this... See: Travle
 clipboard.on('success', function (e) {
   console.info('Action:', e.action);
   console.info('Text:', e.text);
